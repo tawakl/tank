9290b289763f73fb6d31921c4acd596d952b564c
tawakl <tawakl.at@gmail.com>
2022-09-20 15:15:32 +0200

    fix tany

---
+++ app/Blog/Users/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Blog\Users;
+
+use App\Blog\Posts\Post;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'about',
+        'mobile_number',
+        'image',
+        'provider_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function isAdmin(){
+        return $this->role == 'admin';
+    }
+
+//    public function setPasswordAttribute($value)
+//    {
+//        if (trim($value)) {
+//            $this->attributes['password'] = bcrypt(trim($value));
+//        }
+//    }
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+}
