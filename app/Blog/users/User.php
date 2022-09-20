010d3eb75c4117224187bfcf944169377c301a43
tawakl <tawakl.at@gmail.com>
2022-09-20 15:05:29 +0200

    fix

--- app/Blog/users/User.php
+++ app/Blog/users/User.php
@@ -3,7 +3,6 @@
 namespace App\Blog\users;
 
 use App\Blog\Posts\Post;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
